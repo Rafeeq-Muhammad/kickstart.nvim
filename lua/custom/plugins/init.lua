9081899d9b2ea8116c0862935de6d1948959792e
Rafeeq Muhammad <rafeeqnmuhammad@gmail.com>
2025-04-02 19:02:22 -0700

    import custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,43 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- {
+	-- 	"gioele/vim-autoswap",
+	-- 	config = function()
+	-- 		-- Enable tmux support if you use tmux
+	-- 		vim.g.autoswap_detect_tmux = 1
+	-- 	end,
+	-- },
+	{
+		"okuuva/auto-save.nvim",
+		version = "^1.0.0",
+		cmd = "ASToggle",
+		event = { "InsertLeave", "TextChanged" },
+		opts = {
+			-- Your config goes here or leave it empty
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					-- numbers = "buffer_id", -- display buffer IDs
+					diagnostics = "nvim_lsp", -- show lsp diagnostics in the bufferline
+					separator_style = "slant",
+					show_close_icon = false,
+					show_buffer_close_icons = false, -- Add this to disable close icons on buffers
+					offsets = {
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			})
+		end,
+	},
+}

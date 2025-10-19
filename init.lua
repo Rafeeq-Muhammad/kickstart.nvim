f89672986a65d8c1f7bc9e8f659288d626d510ac
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-10-19 16:35:16 -0700

    copy file path

--- init.lua
+++ init.lua
@@ -171,6 +171,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<leader>cfp', function()
+  vim.fn.setreg('+', vim.fn.expand '%:p')
+  vim.notify('Copied file path: ' .. vim.fn.expand '%:p')
+end, { desc = '[C]opy [F]ile [P]ath' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.

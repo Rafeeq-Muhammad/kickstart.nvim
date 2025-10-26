7a05fa51e07d56bbb19bb17a07a6e0f8d58bcb68
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-10-26 12:06:37 -0700

    require custom

--- init.lua
+++ init.lua
@@ -173,7 +173,8 @@ vim.o.confirm = true
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-local shell_popup = require 'custom.shell_popup'
+-- Load custom shell popup helpers (local automation/logic)
+require 'custom.shell_popup'
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })

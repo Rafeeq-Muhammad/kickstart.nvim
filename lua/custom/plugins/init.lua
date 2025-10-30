7bb6bb2c9792535604e9e2dd2cf67d2eb9f0a29e
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-10-29 22:22:03 -0700

    fix formatting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,66 +3,66 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	-- {
-	-- 	"gioele/vim-autoswap",
-	-- 	config = function()
-	-- 		-- Enable tmux support if you use tmux
-	-- 		vim.g.autoswap_detect_tmux = 1
-	-- 	end,
-	-- },
-	{
-		"okuuva/auto-save.nvim",
-		version = "^1.0.0",
-		cmd = "ASToggle",
-		event = { "InsertLeave", "TextChanged" },
-		opts = {
-			-- Your config goes here or leave it empty
-		},
-	},
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("bufferline").setup({
-				options = {
-					-- numbers = "buffer_id", -- display buffer IDs
-					diagnostics = "nvim_lsp", -- show lsp diagnostics in the bufferline
-					separator_style = "slant",
-					show_close_icon = false,
-					show_buffer_close_icons = false, -- Add this to disable close icons on buffers
-					offsets = {
-						filetype = "NvimTree",
-						text = "File Explorer",
-						highlight = "Directory",
-						text_align = "left",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"tpope/vim-sleuth",
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		config = function()
-			require("kanagawa").setup({
-				theme = "wave",
-			})
-			vim.cmd.colorscheme("kanagawa-wave")
-		end,
-	},
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			local notify = require("notify")
-			notify.setup({
-				stages = "fade_in_slide_out",
-				timeout = 3000,
-			})
-			vim.notify = notify
-		end,
-	},
+  -- {
+  -- 	"gioele/vim-autoswap",
+  -- 	config = function()
+  -- 		-- Enable tmux support if you use tmux
+  -- 		vim.g.autoswap_detect_tmux = 1
+  -- 	end,
+  -- },
+  {
+    'okuuva/auto-save.nvim',
+    version = '^1.0.0',
+    cmd = 'ASToggle',
+    event = { 'InsertLeave', 'TextChanged' },
+    opts = {
+      -- Your config goes here or leave it empty
+    },
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bufferline').setup {
+        options = {
+          -- numbers = "buffer_id", -- display buffer IDs
+          diagnostics = 'nvim_lsp', -- show lsp diagnostics in the bufferline
+          separator_style = 'slant',
+          show_close_icon = false,
+          show_buffer_close_icons = false, -- Add this to disable close icons on buffers
+          offsets = {
+            filetype = 'NvimTree',
+            text = 'File Explorer',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      }
+    end,
+  },
+  {
+    'tpope/vim-sleuth',
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    config = function()
+      require('kanagawa').setup {
+        theme = 'wave',
+      }
+      vim.cmd.colorscheme 'kanagawa-wave'
+    end,
+  },
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      local notify = require 'notify'
+      notify.setup {
+        stages = 'fade_in_slide_out',
+        timeout = 3000,
+      }
+      vim.notify = notify
+    end,
+  },
 }

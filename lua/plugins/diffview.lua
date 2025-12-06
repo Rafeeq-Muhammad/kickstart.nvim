98d9b80f7279e7f4f2f1e02ac88fca153409b487
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-12-06 11:32:31 -0800

    diffview keymaps

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,5 +2,12 @@ return {
   {
     'sindrets/diffview.nvim',
     cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles', 'DiffviewFileHistory' },
+    keys = {
+      { '<leader>dO', '<cmd>DiffviewOpen<cr>', desc = 'Diffview [O]pen' },
+      { '<leader>dC', '<cmd>DiffviewClose<cr>', desc = 'Diffview [C]lose' },
+      { '<leader>dt', '<cmd>DiffviewToggleFiles<cr>', desc = 'Diffview [t]oggle sidebar' },
+      { '<leader>dc', '<cmd>DiffviewOpen HEAD~1<cr>', desc = 'Diffview current [c]ommit vs parent commit' },
+      { '<leader>dh', '<cmd>DiffviewOpen HEAD<cr>', desc = 'Diffview current commit vs [h]ead' },
+    },
   },
 }

d3035884f3d3328d2e20c942aaa06c7cbc2c18b3
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-12-06 14:41:35 -0800

    lazy git

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- Setting the keymap for the entry point
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}

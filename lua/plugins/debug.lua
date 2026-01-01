b5a22a0a579acf57dec77759bf230b7e880709da
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2026-01-01 12:16:29 -0800

    dap python

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -72,9 +72,9 @@ return {
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
-      automatic_installation = true,
+ 
       handlers = {},
-      ensure_installed = { 'codelldb' },
+      ensure_installed = { 'codelldb', 'python' },
     }
 
     dapui.setup {
@@ -122,5 +122,24 @@ return {
     }
     dap.configurations.c = dap.configurations.cpp
     dap.configurations.rust = dap.configurations.cpp
+
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',
+        pythonPath = function()
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+            return cwd .. '/venv/bin/python'
+          elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+            return cwd .. '/.venv/bin/python'
+          else
+            return '/usr/bin/python3'
+          end
+        end,
+      },
+    }
   end,
 }
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,11 +3,11 @@ return {
     'sindrets/diffview.nvim',
     cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles', 'DiffviewFileHistory' },
     keys = {
-      { '<leader>dO', '<cmd>DiffviewOpen<cr>', desc = 'Diffview [O]pen' },
-      { '<leader>dC', '<cmd>DiffviewClose<cr>', desc = 'Diffview [C]lose' },
-      { '<leader>dt', '<cmd>DiffviewToggleFiles<cr>', desc = 'Diffview [t]oggle sidebar' },
-      { '<leader>dc', '<cmd>DiffviewOpen HEAD~1<cr>', desc = 'Diffview current [c]ommit vs parent commit' },
-      { '<leader>dh', '<cmd>DiffviewOpen HEAD<cr>', desc = 'Diffview current commit vs [h]ead' },
+      { '<leader>DO', '<cmd>DiffviewOpen<cr>', desc = 'Diffview [O]pen' },
+      { '<leader>DC', '<cmd>DiffviewClose<cr>', desc = 'Diffview [C]lose' },
+      { '<leader>Dt', '<cmd>DiffviewToggleFiles<cr>', desc = 'Diffview [t]oggle sidebar' },
+      { '<leader>Dc', '<cmd>DiffviewOpen HEAD~1<cr>', desc = 'Diffview current [c]ommit vs parent commit' },
+      { '<leader>Dh', '<cmd>DiffviewOpen HEAD<cr>', desc = 'Diffview current commit vs [h]ead' },
     },
     opts = {
       hooks = {
